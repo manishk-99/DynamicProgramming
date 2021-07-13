a00c28ba387ebed0e7a3e5be38ebae88ba9badf7
Manish Kumar <manish.hit2800@gmail.com>
2021-07-13 18:21:49 +0530

    CountOfSubsetSum-Modified

--- CountOfSubsetSum.cpp
+++ CountOfSubsetSum.cpp
@@ -30,14 +30,14 @@ int countOfSubsetSum(int arr[], int n, int sum)
             }
         }
     }
-    for(int i=0;i<n+1;i++)
-    {
-        for(int j=0;j<sum+1;j++)
-        {
-            cout<<dp[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    // for(int i=0;i<n+1;i++)
+    // {
+    //     for(int j=0;j<sum+1;j++)
+    //     {
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     return dp[n][sum];
 }
 
